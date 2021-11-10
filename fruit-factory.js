4ad806db40bc9bea44e147fd0f80d7e3500525ca
Kim Frans <kimfrans34@gmail.com>
2021-11-10 10:22:11 +0200

    tests created and working

--- fruit-factory.js
+++ fruit-factory.js
@@ -3,32 +3,43 @@ const { Pool } = require("pg");
 module.exports = function fruitBaskets(pool) {
 
     async function addingNewBasket(fruitName, fruitQuantity, fruitPrice){
-        console.log(typeof fruitName, typeof fruitQuantity, typeof fruitPrice);
+        // await pool.query("INSERT INTO fruit_basket(fruit_name, quantity, price) values($1, $2, $3)", [fruitName, fruitQuantity,fruitPrice])
+        // console.log(typeof fruitName, typeof fruitQuantity, typeof fruitPrice);
 
-        const dbFruit = await pool.query('SELECT * FROM fruit_basket WHERE fruit_name = $1', [fruitName]);
+        const dbFruit = await pool.query('select * from fruit_basket');
         if(dbFruit.rows.length === 0){
             await pool.query("INSERT INTO fruit_basket(fruit_name, quantity, price) values($1, $2, $3)", [fruitName, fruitQuantity,fruitPrice])
         }
-      await updateFruitCount(fruitName)
-        console.log(dbFruit)
+        // else{
+        //     await updateFruitCount(fruitName)
+        // }
+      
+        // console.log(dbFruit)
     }
 
     async function findingBasketOfFruit(fruit){
-        const findFruit = await pool.query('SELECT * fruit_basket WHERE fruit_name = $1', [fruit])
-        // console.log(findFruit.rows[0]);
+        const findFruit = await pool.query('SELECT fruit_name, quantity, price FROM fruit_basket WHERE fruit_name = $1', [fruit])
+                                            
+        // console.log(findFruit.rows);
         return findFruit.rows
     }
 
-    async function updateFruitCount(fruitName){
-        const updateCount = await pool.query('UPDATE fruit_basket SET quantity = quantity + 1 WHERE fruit_name = $1', [fruitName])
+    async function updateFruitCount(fruitQuantity, fruitName){
+        
+        const updateCount = await pool.query('UPDATE fruit_basket SET quantity = quantity + $1 WHERE fruit_name = $2', [fruitQuantity, fruitName])
+        // console.log(updateCount.rows[0].quantity);
+        return updateCount.rows
     }
 
     async function totalFruitCount(singleFruit){
-        const totalCount = await pool.query('SELECT quantity from fruit_basket WHERE fruit_name = $1', [singleFruit])
+        const totalCount = await pool.query('SELECT quantity FROM fruit_basket WHERE fruit_name = $1', [singleFruit])
+        // console.log(totalCount);
+        return totalCount.rows
     }
 
     async function totalFruitBaskets(totalFruit){
-        const totalCount = await pool.query('SELECT sum(quantity) fruit_basket WHERE fruit_name = $1', [totalFruit])
+        const totalCount = await pool.query('SELECT (quantity*price) AS total FROM fruit_basket WHERE fruit_name = $1', [totalFruit])
+        return totalCount.rows
     }
 
 
--- test/fruit.test.js
+++ test/fruit.test.js
@@ -16,14 +16,37 @@ describe('The fruit basket web app', function () {
 
     beforeEach(async function () {
         // clean the tables before each test run
-        await pool.query("delete from fruit_basket");
+        await pool.query("delete from fruit_basket;");
+    
     });
 
-    it('should add a new fruit basket', async function () {
+    it('should add a new fruit basket and find the fruit', async function () {
         await fruitTest.addingNewBasket("Peaches", 5, 3)
-        await fruitTest.findingBasketOfFruit("Peaches")
 
-        assert.equal(["Peaches", 5, 3], await fruitTest.findingBasketOfFruit("Peaches"));
+        assert.deepEqual([{fruit_name: "Peaches", quantity: 5, price: 3}], await fruitTest.findingBasketOfFruit("Peaches"));
+
+    });
+    it('should update the fruit count', async function () {
+        await fruitTest.addingNewBasket("Strawberries", 5, 3)
+        await fruitTest.addingNewBasket("Apples", 10, 5)
+        
+        await fruitTest.updateFruitCount(5, "Strawberries")
+
+        assert.deepEqual([{fruit_name: "Strawberries", price: '3.00', quantity: 10}], await fruitTest.findingBasketOfFruit("Strawberries"));
+
+    });
+    it('should get the sum of a single fruit basket', async function () {
+        await fruitTest.addingNewBasket("Peaches", 5, 3)
+        
+
+        assert.deepEqual([{quantity: 5}], await fruitTest.totalFruitCount('Peaches'));
+
+    });
+    it('should show the sum of the total of the fruit baskets for a given fruit type', async function () {
+        await fruitTest.addingNewBasket("Grapes", 10, 5)
+        // await fruitTest.addingNewBasket("Grapes", 6, 8)
+
+        assert.deepEqual([{total: 50.00}], await fruitTest.totalFruitBaskets('Grapes'));
 
     });
 
